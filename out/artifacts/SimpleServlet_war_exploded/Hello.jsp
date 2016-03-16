e7999449408597015e5c75f1a3727dd07f503606
Thippeswamy <tethippe@visa.com>
2016-03-16 14:37:59 -0700

    Simple Servlet

---
+++ out/artifacts/SimpleServlet_war_exploded/Hello.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: tethippe
+  Date: 3/16/2016
+  Time: 2:06 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello</title>
+</head>
+<body>
+<%
+    out.println("Hello World");
+%>
+</body>
+</html>
---
+++ src/com/mykong/SimpleServlet.java
@@ -0,0 +1,28 @@
+package com.mykong;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+/**
+ * Created by tethippe on 3/16/2016.
+ */
+@WebServlet("/home")
+public class SimpleServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>hi!</h1>");
+        out.println(LocalDateTime.now().toString());
+        out.flush();
+    }
+}
